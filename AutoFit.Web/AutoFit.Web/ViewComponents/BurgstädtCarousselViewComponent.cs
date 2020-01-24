fbb16772827def63ffce6bd8ac7750be4b7425b4
rs92vegi <robin.seidel@hypservice.de>
2020-01-24 11:40:45 +0100

    [fix] fix footer width

--- AutoFit.Web/AutoFit.Web/ViewComponents/BurgstädtCarousselViewComponent.cs
+++ AutoFit.Web/AutoFit.Web/ViewComponents/BurgstädtCarousselViewComponent.cs
@@ -2,8 +2,6 @@
 using AutoFit.Web.ViewModels.Files;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace AutoFit.Web.ViewComponents
