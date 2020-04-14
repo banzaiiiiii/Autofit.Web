661099260ac3a9c06d5754041fbeea0e3c6b3b35
rs92vegi <robin.seidel@hypservice.de>
2020-04-14 22:45:44 +0200

    hide/show files in folder

--- AutoFit.Web/AutoFit.Web/wwwroot/js/site.js
+++ AutoFit.Web/AutoFit.Web/wwwroot/js/site.js
@@ -5,3 +5,14 @@ var $scroll_div = $('#scroll_top_wrap');
 $(document).scroll(function () {
     $scroll_div.css({ display: $(this).scrollTop() > 100 ? "block" : "none" });
 });
+
+
+function HideShowFilesInContainer() {
+    if ($(".FolderFiles").is(":visible")){
+        $(".FolderFiles").hide();
+    } else {
+        $(".FolderFiles").show();
+    }
+    
+}
+
